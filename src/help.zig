bc8e67946e61714af9b09f9ffd8aea4fd675d52f
Konstantin Tarasov <ktarasov@peredelka-saitov.ru>
2026-09-02 16:41:49 +0300

    fix(help): added colors for argements

--- src/help.zig
+++ src/help.zig
@@ -43,9 +43,9 @@ pub fn generateHelpWithConfig(allocator: std.mem.Allocator, spec: CommandSpec, u
         if (arg.positional) {
             if (arg.hidden) continue;
             if (arg.required) {
-                try writer.print(" <{s}>", .{arg.name});
+                try writer.print(" <{s}{s}{s}>", .{ argument, arg.name, reset });
             } else {
-                try writer.print(" [{s}]", .{arg.name});
+                try writer.print(" [{s}{s}{s}]", .{ argument, arg.name, reset });
             }
         }
     }
@@ -77,9 +77,9 @@ pub fn generateHelpWithConfig(allocator: std.mem.Allocator, spec: CommandSpec, u
                     if (sub_arg.hidden) continue;
                     if (sub_arg.positional) {
                         if (sub_arg.required) {
-                            try writer.print(" {s}<{s}>{s}", .{ argument, sub_arg.name, reset });
+                            try writer.print(" <{s}{s}{s}>", .{ argument, sub_arg.name, reset });
                         } else {
-                            try writer.print(" {s}[{s}]{s}", .{ argument, sub_arg.name, reset });
+                            try writer.print(" [{s}{s}{s}]", .{ argument, sub_arg.name, reset });
                         }
                     }
                 }
